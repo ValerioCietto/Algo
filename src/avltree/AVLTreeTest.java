4222f8d794a1e5b529e0452e514a7fbcc2ffa53e
st116633 <cietto.valerio@educ.di.unito.it>
2014-12-02 14:41:09 +0100

    avltreetest

---
+++ src/avltree/AVLTreeTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package avltree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author valerio
+ *
+ */
+public class AVLTreeTest {
+
+	/**
+	 * Test method for {@link avltree.AVLTree#get(int)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#isEmpty()}.
+	 */
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#AVLTree()}.
+	 */
+	@Test
+	public void testAVLTree() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#printInOrder()}.
+	 */
+	@Test
+	public void testPrintInOrder() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#is1Balanced()}.
+	 */
+	@Test
+	public void testIs1Balanced() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#put(avltree.Elem)}.
+	 */
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link avltree.AVLTree#remove(int)}.
+	 */
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
