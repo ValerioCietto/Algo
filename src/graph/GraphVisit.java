25295da98f2ae1a99123b52454dd5e01f4fb85aa
st116633 <cietto.valerio@educ.di.unito.it>
2014-12-09 16:44:59 +0100

    grafi

--- src/graph/GraphVisit.java
+++
@@ -1,7 +0,0 @@
-package graph;
-
-
-public interface GraphVisit<V, E> {
-	Graph<V,E> breadthFirst(Graph<V, E> graph, V s, VertexAnalyser<V> va);
-	Graph<V,E> depthFirst(Graph<V, E> graph, V s, VertexAnalyser<V> va);
-}
