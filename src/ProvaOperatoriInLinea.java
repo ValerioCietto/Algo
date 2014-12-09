b22b14a95265a7434c3c25df8f22bc267e262f0a
st116633 <cietto.valerio@educ.di.unito.it>
2014-12-09 16:42:53 +0100

    Impariamo gli operatori ternari insieme!

---
+++ src/ProvaOperatoriInLinea.java
@@ -0,0 +1,10 @@
+
+public class ProvaOperatoriInLinea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1==2);
+		System.out.println((1==1) ? true : false);
+	}
+
+}
