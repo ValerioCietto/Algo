dd1f30cbfe9b38b745ab16991f0003d4fe3f4f41
st116633 <cietto.valerio@educ.di.unito.it>
2015-01-11 21:28:18 +0100

    javadocs!

--- src/graph/DenseGraph.java
+++ src/graph/DenseGraph.java
@@ -17,13 +17,20 @@ public class DenseGraph<V, E> implements Graph{
 	private int n;//numero di nodi
 	private int m;//numero di archi
 	
+	/**
+	 * inizializza un grafo vuoto
+	 */
 	public DenseGraph(){
 		nodi = new ArrayList<V>();
 		archi = new HashMap<V, ArrayList<Arco<V,E>>>();
 		n = 0;
 		m = 0;
 	}
-	
+	/**
+	 * Aggiunge un vertice se non è già presente
+	 * @param vertex oggetto da aggiungere
+	 * @return true se è stato inserito, false se già presente
+	 */
 	@Override
 	public boolean addVertex(Object vertex) {
 		if(!nodi.contains(vertex)){
@@ -35,7 +42,13 @@ public class DenseGraph<V, E> implements Graph{
 		}
 		return false;
 	}
-
+	/**
+	 * aggiunge un arco al grafo se non è già presente
+	 * @param v1 vertice da cui esce l'arco
+	 * @param v2 vertice in cui entra l'arco
+	 * @param info nome arco o altro
+	 * @return true se è stato inserito, false altrimenti
+	 */
 	@Override
 	public boolean addEdge(Object v1, Object v2, Object info) {
 		
@@ -49,7 +62,15 @@ public class DenseGraph<V, E> implements Graph{
 		
 		
 	}
-
+	/**
+	 * aggiunge un arco al grafo se non è già presente
+	 * aggiunge anche il peso.
+	 * @param v1
+	 * @param v2
+	 * @param weight
+	 * @param info
+	 * @return true se è stato inserito, false altrimenti
+	 */
 	@Override
 	public boolean addEdge(Object v1, Object v2, double weight, Object info) {
 		if(nodi.contains(v1)){
@@ -64,29 +85,53 @@ public class DenseGraph<V, E> implements Graph{
 		}
 		return false;
 	}
-
+	/**
+	 * aggiunge un arco in due direzioni
+	 * al grafo tra v1 e v2, se non è già presente
+	 * @param v1
+	 * @param v2
+	 * @param info
+	 * @return true se almeno un arco è stato inserito,
+	 *  false altrimenti
+	 */
 	@Override
 	public boolean addUndirectedEdge(Object v1, Object v2, Object info) {
-		addEdge(v1,v2,0.0,info);
-		addEdge(v2,v1,0.0,info);
-		return false;
+		boolean arco1 = addEdge(v1,v2,0.0,info);
+		boolean arco2 = addEdge(v2,v1,0.0,info);
+		return arco1||arco2;
 	}
-
+	/**
+	 * aggiunge un arco in due direzioni
+	 * al grafo tra v1 e v2, se non è già presente
+	 * @param v1
+	 * @param v2
+	 * @param double
+	 * @param info
+	 * @return true se almeno un arco è stato inserito,
+	 *  false altrimenti
+	 */
 	@Override
 	public boolean addUndirectedEdge(Object v1, Object v2, double weight,
 			Object info) {
-		addEdge(v1,v2,weight,info);
-		addEdge(v2,v1,weight,info);
-		return false;
+		boolean arco1 = addEdge(v1,v2,weight,info);
+		boolean arco2 = addEdge(v2,v1,weight,info);
+		return arco1||arco2;
 	}
-
+    /**
+     * controlla se un vertice esiste
+     * necessario perchè nodi è privato
+     * @return true se esiste, false altrimenti
+     */
 	@Override
 	public boolean hasVertex(Object vertex) {
 		if(nodi.contains(vertex))
 			return true;
 		return false;
 	}
-
+    /**
+     * controlla se un arco tra v1 e v2 esiste
+     * @return true se esiste, false altrimenti
+     */
 	@Override
 	public boolean hasEdge(Object v1, Object v2) {
 		if(nodi.contains(v1)){
@@ -98,12 +143,20 @@ public class DenseGraph<V, E> implements Graph{
 		}
 		return false;
 	}
+	/**
+	 * @return peso dell'arco tra vertice source e vertice dest
+	 */
 	@Override
 	public double getWeight(Object source, Object dest) {
 		return getEdge((V)source,(V)dest).getWeight();
 		
 	}
-
+    /**
+     * Ottiene un oggetto arco generico
+     * @param source
+     * @param dest
+     * @return un oggetto di tipo Arco
+     */
 	private Arco<V, E> getEdge(V source, V dest) {
 		ArrayList<Arco<V, E>> neighbors = new ArrayList<Arco<V, E>>();
 		neighbors = (ArrayList<Arco<V, E>>)archi.get(source);
@@ -117,7 +170,17 @@ public class DenseGraph<V, E> implements Graph{
 		}
 		return null;
 	}
-
+    /**
+     * ottiene una lista dei nodi adiacenti
+     */
+	public ArrayList neighbors(Object vertex) {
+		if(nodi.contains(vertex)){
+			ArrayList<Arco<V,E>> neighbors = (ArrayList<Arco<V,E>>)archi.get(vertex);
+			return neighbors;
+		}
+		return null;
+	}
+	
 	@Override
 	public ArrayList vertices() {
 		return nodi;
@@ -128,14 +191,6 @@ public class DenseGraph<V, E> implements Graph{
 	}
 	public HashMap getArchi(){
 		return archi;
-	}
-	
-	public ArrayList neighbors(Object vertex) {
-		if(nodi.contains(vertex)){
-			ArrayList<Arco<V,E>> neighbors = (ArrayList<Arco<V,E>>)archi.get(vertex);
-			return neighbors;
-		}
-		return null;
 	}
 	
 	public int getNumberOfArcs(){
